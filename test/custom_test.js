b67d7c49c26d5a122ae91166129cefefe4d6fd82
Karolis Narkevicius <karolis.n@gmail.com>
2014-08-30 12:51:17 +0100

    A working version with tests

---
+++ test/custom_test.js
@@ -0,0 +1,66 @@
+var rc = require("../");
+var path = require("path");
+var expect = require("chai").expect;
+
+var _ = require("lodash");
+var when = require("when");
+var path = require("path");
+var resolveModule = require("resolve");
+var uid = require("../lib/uid");
+
+var output = {
+  map: {
+    'backbone@1.1.2': {
+      underscore: 'underscore@1.7.0'
+    },
+    '*': {
+      'underscore@>=1.5.0': 'underscore@1.7.0'
+    }
+  },
+  packages: [{
+    name: 'backbone@1.1.2',
+    main: 'backbone.js',
+    location: 'pkg/backbone@1.1.2'
+  }, {
+    name: 'underscore@1.7.0',
+    main: 'underscore.js',
+    location: 'pkg/underscore@1.7.0'
+  }]
+};
+
+describe("rc.npm", function () {
+  it("generates configuration for an npm based project", function () {
+    var root = path.join(__dirname, "..");
+    return rc.generate({
+      root: root,
+      dependencies: readDependencies(root),
+      resolve: resolve,
+      location: location
+    }).then(function (config) {
+      expect(config).to.deep.equal(output);
+    });
+  });
+});
+
+function readDependencies(path) {
+  return _.reduce(require(path + "/package.json").dependencies, function (memo, version, dep) {
+    if (dep === "backbone") {
+      memo.push(dep + "@" + version);
+    }
+    return memo;
+  }, []);
+}
+
+function resolve(name, version, basedir) {
+  return when.promise(function (resolve, reject) {
+    resolveModule(name + "/package.json", {basedir: basedir}, function (err, location) {
+      if (err) return reject(err);
+      resolve(path.dirname(location));
+    });
+  });
+}
+
+function location(manifest) {
+  return "pkg/" + uid.create(manifest.name, manifest.version);
+}
+
---
+++ test/npm_test.js
@@ -0,0 +1,56 @@
+var rc = require("../");
+var path = require("path");
+var expect = require("chai").expect;
+
+var output = {
+  map: {
+    'backbone@1.1.2': {
+      underscore: 'underscore@1.7.0'
+    },
+    '*': {
+      'backbone': 'backbone@1.1.2',
+      'lodash': 'lodash@^2.4.1',
+      'lodash@^2.4.1': 'lodash@2.4.1',
+      'ramda': 'ramda@^0.4.0',
+      'ramda@^0.4.0': 'ramda@0.4.0',
+      "resolve": "resolve@^1.0.0",
+      'resolve@^1.0.0': 'resolve@1.0.0',
+      'when@^3.4.4': 'when@3.4.4',
+      'when': 'when@^3.4.4',
+      'underscore@>=1.5.0': 'underscore@1.7.0'
+    }
+  },
+  packages: [{
+    name: 'backbone@1.1.2',
+    main: 'backbone.js',
+    location: 'node_modules/backbone'
+  }, {
+    name: 'lodash@2.4.1',
+    main: 'dist/lodash.js',
+    location: 'node_modules/lodash'
+  }, {
+    name: 'ramda@0.4.0',
+    main: 'ramda.js',
+    location: 'node_modules/ramda'
+  }, {
+    name: 'resolve@1.0.0',
+    main: 'index.js',
+    location: 'node_modules/resolve'
+  }, {
+    name: 'when@3.4.4',
+    main: 'when',
+    location: 'node_modules/when'
+  }, {
+    name: 'underscore@1.7.0',
+    main: 'underscore.js',
+    location: 'node_modules/backbone/node_modules/underscore'
+  }]
+};
+
+describe("rc.npm", function () {
+  it("generates configuration for an npm based project", function () {
+    return rc.npm(path.join(__dirname, "..")).then(function (config) {
+      expect(config).to.deep.equal(output);
+    });
+  });
+});
